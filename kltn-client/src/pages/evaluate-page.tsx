1d2051dbdbd5b77d3cebaa06878092906481554e
Satoh Jiro <trananhq2345@gmail.com>
2023-04-15 12:57:30 +0700

    clean variable not use

--- kltn-client/src/pages/evaluate-page.tsx
+++ kltn-client/src/pages/evaluate-page.tsx
@@ -1,60 +1,12 @@
-import {
-  Input,
-  Button,
-  message,
-  Upload,
-  Select,
-  Col,
-  Row,
-  Divider,
-} from "antd";
-import type { UploadProps } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
-import { Editor } from "react-draft-wysiwyg";
+import { Input, Button, Select, Col, Row, Divider } from "antd";
+
 import "../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { PlusOutlined } from "@ant-design/icons";
-import {
-  Form,
-  Radio,
-  Cascader,
-  DatePicker,
-  InputNumber,
-  TreeSelect,
-  Switch,
-  Checkbox,
-} from "antd";
-import { useState } from "react";
+
+import { Form, Radio } from "antd";
 
 const { TextArea } = Input;
-const { RangePicker } = DatePicker;
-
-const props: UploadProps = {
-  name: "file",
-  action: "https://www.mocky.io/v2/5cc8019d300000980a055e76",
-  headers: {
-    authorization: "authorization-text",
-  },
-  onChange(info) {
-    if (info.file.status !== "uploading") {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === "done") {
-      message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
 
 const CounterArgumentFrom = (): JSX.Element => {
-  const [componentDisabled, setComponentDisabled] = useState<boolean>(true);
-  const onChange = (value: string) => {
-    console.log(`selected ${value}`);
-  };
-
-  const onSearch = (value: string) => {
-    console.log("search:", value);
-  };
   return (
     <div className="bg-white h-full flex justify-around items-center flex-col p-2 mt-2 ml-2">
       <div className="w-full p-5">
